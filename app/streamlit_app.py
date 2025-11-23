255d0dbd2ceddf884b7d607a78cece9ec13486f3
SUJNYAN KINI <sujnyankini17@gmail.com>
2025-11-23 15:53:08 +0530

    Create streamlit_app.py

---
+++ app/streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from sidebar import display_sidebar
+from chat_interface import display_chat_interface
+
+st.title("Langchain RAG Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = None
+
+
+display_sidebar()
+
+
+display_chat_interface()
